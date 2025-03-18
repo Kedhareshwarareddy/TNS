216160d6a5dcfa511074f85666b0895b4c1f1ddc
kedharreddy <kedharreddy@localhost>
2025-03-18 18:44:54 +0530

    new commit encap

---
+++ TNSBASICS/src/TNSBASICS/Encap_Main.java
@@ -0,0 +1,18 @@
+package TNSBASICS;
+
+
+public class Encap_Main {
+
+	public static void main(String[] args) {
+		
+		// Object creation
+		EncapDemo obj = new EncapDemo(); // new is keywords which is used to create a object
+		obj.setSerialNum(101);
+		obj.setName("Bunny");
+		obj.setAge(15);
+		System.out.println(obj);
+		
+	}
+
+}
+
